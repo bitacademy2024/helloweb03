1233a9c529172123da6fe272c18160f3d0e28d0d
BIT <BIT@DESKTOP-V9BD38C>
2024-04-11 17:44:06 +0900

    spring 1

--- src/main/java/com/bit2024/helloweb/controller/HelloController.java
+++ src/main/java/com/bit2024/helloweb/controller/HelloController.java
@@ -1,13 +1,40 @@
 package com.bit2024.helloweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+
+	public void babo() {
+	}
 	
 	@RequestMapping("/hello")
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println(name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String n) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		mav.addObject("name", n);
+		
+		return mav;
+	}
+	
+	@RequestMapping("/hello4")
+	public String hello4(String n, Model model) {
+		model.addAttribute("name", n);
+		return "/WEB-INF/views/hello3.jsp";
+	}	
+	
 }
